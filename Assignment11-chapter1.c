cddaf80fd2c57745a6efc2e8baec9233ef18d9f8
gagan0183 <g.deepsingh1@gmail.com>
2018-10-20 13:11:04 +0530

    assignment

---
+++ Assignment11-chapter1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main() {
+	float centigrades;
+	float fahrenheit;
+	centigrades = 50;
+	fahrenheit = ((9 * centigrades)/5) + 32;
+	printf("The temperature");
+	printf("%f", centigrades);
+	printf("Celsius equals");
+	printf("%f", fahrenheit);
+	printf("fahrenheit");
+}
